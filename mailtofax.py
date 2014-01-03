c2fa0f09ef09e0e71a5040a8dc458e175ac87d53
Fred Wenzel <fwenzel@mozilla.com>
2014-01-03 13:31:29 -0800

    Bounce mails if they are invalid. Fix #5. L10n. Fix #6.

--- mailtofax.py
+++ mailtofax.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import email
+import json
 import mimetypes
 import re
+import smtplib
 import sys
 import tempfile
+from email.mime.message import MIMEMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from optparse import OptionParser
 from subprocess import call
 
@@ -14,6 +19,70 @@ import settings
 class InputError(Exception):
     """A general error for all the things we can't handle"""
     pass
+
+
+_L10N_CACHE = None
+def _(key):
+    """Poor man's L10n."""
+    global _L10N_CACHE
+
+    if _L10N_CACHE is None:
+        _L10N_CACHE = json.loads(open('l10n.json').read())
+
+    return _L10N_CACHE[settings.LANG].get(key)
+
+
+class Bouncer(object):
+    """Bounce mail generator."""
+
+    def __init__(self, incoming=None):
+        self._msg = MIMEMultipart('alternative')
+        # Hold on to incoming mail so we can use its info to generate bounces.
+        self._incoming = incoming
+
+    def bounce(self, err='Error!', recipient=None):
+        """Generate a bounce message with the given error message."""
+        text = _('email_text') % err
+        html = _('email_html') % err
+
+        self._msg.attach(MIMEText(text, 'plain'))
+        self._msg.attach(MIMEText(html, 'html'))
+
+        # Stick it all in an envelope.
+        envelope = MIMEMultipart('mixed')
+        envelope.attach(self._msg)
+
+        # If present, attach the entire incoming email.
+        if self._incoming is not None:
+            inc_msg = MIMEMessage(self._incoming)
+            inc_msg.add_header('Content-Disposition', 'attachment',
+                               filename='%s.eml' % self._incoming['subject'])
+            envelope.attach(inc_msg)
+
+        if recipient is None and self._incoming:
+            recipient = self._incoming.get('reply-to',
+                                           self._incoming.get('from'))
+
+        recipient = 'fwenzel@mozilla.com'
+        envelope['From'] = settings.MAILTOFAX_EMAIL
+        envelope['To'] = recipient
+        envelope['Subject'] = _('subject') % (self._incoming['subject'] if
+                                              self._incoming else '')
+        if self._incoming:
+            envelope['In-Reply-To'] = self._incoming['message-id']
+
+        #print self._msg.as_string()
+        smtp = smtplib.SMTP(settings.SMTP_SERVER['host'],
+                            settings.SMTP_SERVER.get('port') or 25)
+        if settings.SMTP_SERVER['tls']:
+            smtp.starttls()
+        if settings.SMTP_SERVER.get('user'):
+            smtp.login(settings.SMTP_SERVER['user'],
+                       settings.SMTP_SERVER['password'])
+
+        smtp.sendmail(envelope['From'], [envelope['To']],
+                      envelope.as_string())
+        sys.exit(1)
 
 
 class MailToFax(object):
@@ -28,8 +97,8 @@ class MailToFax(object):
         parser.add_option('-c', '--stdin', dest='stdin', action="store_true",
                           help='read data from stdin, not the file argument')
         parser.add_option('-n', '--dry-run', dest='noexec', action="store_true",
-                          help='Test mode: Do not execute command, just display ' \
-                               'what would be run')
+                          help=('Test mode: Do not execute command, just '
+                                'display what would be run'))
         (self.options, self.args) = parser.parse_args()
 
         if self.options.stdin:
@@ -49,7 +118,7 @@ class MailToFax(object):
     def process_email(self, msg):
         """Process email message: fetch attachments to be sent by fax."""
         if not msg.is_multipart():
-            raise InputError('Input needs to be a multipart message!')
+            Bouncer(msg).bounce(_('e_noattachment'))
 
         # Grab email sender.
         self.sender = msg.get('reply-to', msg.get('from', settings.DEFAULT_SENDER))
@@ -63,6 +132,7 @@ class MailToFax(object):
         else:
             fallback_dest = None
 
+        sent = 0  # Number of faxes actually sent.
         for part in msg.walk():
             content_type = part.get_content_type()
             if content_type not in settings.FAX_MIME_TYPES: continue
@@ -74,9 +144,7 @@ class MailToFax(object):
             elif fallback_dest:
                 destination = fallback_dest
             else:
-                raise InputError(
-                    "Attachment file name or subject line must contain the "
-                    "recipient's fax number!")
+                Bouncer(msg).bounce(_('e_nonumber'))
             #print "Destination: %s" % destination
 
             # Prepare file
@@ -89,8 +157,13 @@ class MailToFax(object):
             tmp.flush() # make sure it's not buffered
 
             self.sendfax(tmp, destination)
+            sent += 1
 
             tmp.close()
+
+        # Error handling: No faxes sent?
+        if not sent:
+            Bouncer(msg).bounce(_('e_noattachment'))
 
     def sendfax(self, file, destination):
         """send a fax to the given destination."""
@@ -107,4 +180,3 @@ class MailToFax(object):
 if __name__ == '__main__':
     MailToFax().main()
     # TODO: Report errors back to sender.
-
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 # mailtofax settings file
 
+# Interface lanugage.
+# Check l10n.json for valid choices.
+LANG = 'en'
+
 # This is where the attachments will be temporarily stored. /tmp is probably
 # a good spot.
 TMP = '/tmp'
@@ -9,6 +13,19 @@ FAX_MIME_TYPES = ['application/pdf']
 
 # Default sender, if email sender can't be determined.
 DEFAULT_SENDER = 'postmaster@localhost'
+
+# My email address, for bounces.
+MAILTOFAX_EMAIL = 'Mail To Fax Gateway <mailtofax@localhost>'
+
+# Outgoing SMTP Server (for sending out bounces)
+# For config format, cf. http://docs.python.org/2/library/smtplib.html
+SMTP_SERVER = {
+    'host': 'localhost',
+    #'port': 25,
+    #'user': 'my_username',
+    #'password': 'my_password',
+    #'tls': True,
+}
 
 # sendfax command. Add all options here.
 # Refer to the sendfax man page for more info.
